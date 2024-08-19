b795fbf3665c274146b4be299c066df78fd039ee
panda0815 <brave.panda.po@gmail.com>
2024-08-19 23:16:41 +0700

    add function called getItem

--- contract/contracts/DotaAmazingItem.sol
+++ contract/contracts/DotaAmazingItem.sol
@@ -16,7 +16,7 @@ contract DotaAmazingItem is ERC721, Ownable {
         uint256 id;
         string name;
         string desc;
-        uint16 itemType;
+        uint16 level;
         string imgUrl;
         uint256 price;
         ItemInformation[] informations;
@@ -29,24 +29,24 @@ contract DotaAmazingItem is ERC721, Ownable {
 
     Item[] public items;
 
-    event createdItem(
-        uint256 _id,
+    event mintedItem(
+        uint256 _tokenId,
         string _name,
         string _desc,
-        uint16 _itemType,
+        uint16 _level,
         string _imgUrl,
         uint256 _price,
         ItemInformation[] _informations
     );
 
-    constructor() ERC721("GameItems", "Token") Ownable(msg.sender) {
+    constructor() ERC721("DotaAmazingItem", "Token") Ownable(msg.sender) {
         _currentTokenId = 0;
     }
 
-    function createItem(
+    function mintItem(
         string memory _name,
         string memory _desc,
-        uint16 _itemType,
+        uint16 _level,
         string memory _imgUrl,
         uint256 _price,
         ItemInformation[] memory _informations
@@ -56,10 +56,15 @@ contract DotaAmazingItem is ERC721, Ownable {
 
         items.push();
         Item storage newItem = items[items.length - 1];
+
         newItem.id = tokenId;
         newItem.name = _name;
+        newItem.desc = _desc;
+        newItem.level = _level;
         newItem.imgUrl = _imgUrl;
         newItem.price = _price;
+
+        delete newItem.informations;
         for (uint256 i = 0; i < _informations.length; i++) {
             newItem.informations.push(_informations[i]);
         }
@@ -67,14 +72,44 @@ contract DotaAmazingItem is ERC721, Ownable {
         _tokenExists[tokenId] = true;
         _itemToOwner[tokenId] = msg.sender;
 
-        emit createdItem(
+        _mint(msg.sender, tokenId);
+
+        emit mintedItem(
             tokenId,
             _name,
             _desc,
-            _itemType,
+            _level,
             _imgUrl,
             _price,
             _informations
+        );
+    }
+
+    function getItem(
+        uint256 _tokenId
+    )
+        public
+        view
+        returns (
+            uint256 id,
+            string memory name,
+            string memory desc,
+            uint16 level,
+            string memory imgUrl,
+            uint256 price,
+            ItemInformation[] memory informations
+        )
+    {
+        require(_tokenExists[_tokenId], "This token does not exist");
+        Item storage item = items[_tokenId];
+        return (
+            item.id,
+            item.name,
+            item.desc,
+            item.level,
+            item.imgUrl,
+            item.price,
+            item.informations
         );
     }
 }
