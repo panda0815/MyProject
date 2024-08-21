fc22a207313515f6169a71925916c11a67c5e87f
panda0815 <brave.panda.po@gmail.com>
2024-08-21 23:27:43 +0700

    install and provide chakra-uo

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { CustomThemeProvider } from "@/contexts/CustomThemeContext";
 import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CustomThemeProvider>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </CustomThemeProvider>
   );
 }
